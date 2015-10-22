c2ad679d3f41b84cec48ca3179f221ce3291f500
makidiegoaerox <makidiegoaerox@gmail.com>
2015-10-22 20:22:17 +0200

    Script para Clientes finalizado

--- init_script.sh
+++ init_script.sh
@@ -1,10 +1,14 @@
-#Instalacion de Vbox 4.3 (En prueba)
-#URL='http://download.virtualbox.org/virtualbox/4.3.32/#virtualbox-4.3_4.3.32-103443~Debian~wheezy_amd64.deb'; 
-#FILE='mktemp'; 
-#wget "$URL" -qO $FILE && dpkg -i $FILE;
-#rm $FILE
+#Mensaje de Espera
 
-#Compilacion de Cabeceras (Correcto)
+echo "Realizando las tareas de preparacion , Tardara unos minutos...";
+
+#Instalacion de Vbox 4.3 
+URL='http://download.virtualbox.org/virtualbox/4.3.32/virtualbox-4.3_4.3.32-103443~Debian~wheezy_amd64.deb'; 
+FILE='mktemp'; 
+wget "$URL" -qO $FILE && dpkg -i $FILE;
+rm $FILE
+
+#Compilacion de Cabeceras 
 
 apt-get update 
 apt-get install build-essential -y
@@ -12,26 +16,28 @@ apt-get install linux-headers-$(uname -r) -y
 apt-get -f install -y
 
 
-#/etc/init.d/vboxdrv setup (En prueba)
-
-#Instalacion de Vagrant (Correcto)
+#Instalacion de Vagrant 
 
 apt-get install vagrant -y
 
-#Instalacion de GIT (Correcto)
+#Instalacion de GIT 
 
 apt-get install git -y
 
-#Instalacion de dependencias (Correcto)
+#Instalacion de dependencias 
 
 apt-get -f install -y
 
-#Clonacion desde GITHUB (Correcto)
+#Clonacion desde GITHUB 
 
 mkdir /home/debianasir/temporal
 git clone https://github.com/makidiegoaerox/VagrantServicios.git /home/debianasir/temporal
 
-#Deploy de Vagrant (En Prueba)
+#Compilacion de Kernel para Vbox
 
-#vagrant up /temporal/VagrantServicios
+/etc/init.d/vboxdrv setup 
+
+#Deploy de Vagrant 
+
+(cd /home/debianasir/temporal && vagrant up)
 
